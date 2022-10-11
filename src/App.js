693956ef6eeef54f9855a8f2bcac03493b79b650
Aljay Pascual <aljay.pascual@norimaconsulting.com>
2022-10-11 19:36:20 +0800

    second commiitss

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import './App.css';
 import Footer from './components/Footer';
 import Homepage from './components/Homepage';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import Navbar from './components/Navbar';
 import History from './components/History';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-          <Navbar/>
+      <HashRouter>
+        <Navbar />
         <Routes>
-          <Route path='/' element={<Homepage/>}/>
-          <Route path='/history' element={<History/>}/>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/history" element={<History />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
       <Footer />
     </div>
   );
